44fb0a39128e90be83ffab19a0f3580019f75819
Milan Pavlik <pavlik.mil@gmail.com>
2024-05-02 13:22:47 +0000

    Fix tests

--- writer_test.go
+++ writer_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 	"sync"
 	"testing"
@@ -27,130 +28,130 @@ func TestWriter(t *testing.T) {
 		return dir, cleanup
 	}
 
-	// t.Run("creates target directory if it does not exist", func(t *testing.T) {
-	// 	dir, cleanup := setup(t)
-	// 	defer cleanup()
+	t.Run("creates target directory if it does not exist", func(t *testing.T) {
+		dir, cleanup := setup(t)
+		defer cleanup()
 
-	// 	dir = filepath.Join(dir, "foo")
-	// 	w, err := New(logger, Options{
-	// 		Directory: dir,
-	// 	})
-	// 	require.NoError(t, err)
-	// 	require.NoError(t, w.Close(), "must close writer")
+		dir = filepath.Join(dir, "foo")
+		w, err := New(logger, Options{
+			Directory: dir,
+		})
+		require.NoError(t, err)
+		require.NoError(t, w.Close(), "must close writer")
 
-	// 	f, err := os.Stat(dir)
-	// 	require.NoError(t, err)
-	// 	require.True(t, f.IsDir(), "must create directory")
-	// })
+		f, err := os.Stat(dir)
+		require.NoError(t, err)
+		require.True(t, f.IsDir(), "must create directory")
+	})
 
-	// t.Run("create, write, close", func(t *testing.T) {
-	// 	dir, cleanup := setup(t)
-	// 	defer cleanup()
+	t.Run("create, write, close", func(t *testing.T) {
+		dir, cleanup := setup(t)
+		defer cleanup()
 
-	// 	w, err := New(logger, Options{
-	// 		Directory: dir,
-	// 	})
-	// 	require.NoError(t, err, "must construct writer")
+		w, err := New(logger, Options{
+			Directory: dir,
+		})
+		require.NoError(t, err, "must construct writer")
 
-	// 	message := []byte("message")
-	// 	_, err = w.Write(message)
-	// 	require.NoError(t, err, "write must succeed")
-	// 	require.NoError(t, w.Close(), "must close writer")
+		message := []byte("message")
+		_, err = w.Write(message)
+		require.NoError(t, err, "write must succeed")
+		require.NoError(t, w.Close(), "must close writer")
 
-	// 	files, err := os.ReadDir(dir)
-	// 	require.NoError(t, err)
+		files, err := os.ReadDir(dir)
+		require.NoError(t, err)
 
-	// 	require.Len(t, files, 1, "must write exactly one file")
-	// 	written, err := os.ReadFile(filepath.Join(dir, files[0].Name()))
-	// 	require.NoError(t, err, "must read file")
-	// 	require.Equal(t, message, written)
-	// })
+		require.Len(t, files, 1, "must write exactly one file")
+		written, err := os.ReadFile(filepath.Join(dir, files[0].Name()))
+		require.NoError(t, err, "must read file")
+		require.Equal(t, message, written)
+	})
 
-	// t.Run("rotates on file size", func(t *testing.T) {
-	// 	dir, cleanup := setup(t)
-	// 	defer cleanup()
+	t.Run("rotates on file size", func(t *testing.T) {
+		dir, cleanup := setup(t)
+		defer cleanup()
 
-	// 	max := 128
-	// 	w, err := New(logger, Options{
-	// 		Directory:       dir,
-	// 		MaximumFileSize: int64(max),
-	// 	})
-	// 	require.NoError(t, err)
+		max := 128
+		w, err := New(logger, Options{
+			Directory:       dir,
+			MaximumFileSize: int64(max),
+		})
+		require.NoError(t, err)
 
-	// 	// fill up the first file
-	// 	_, err = w.Write([]byte(strings.Repeat("a", max)))
-	// 	require.NoError(t, err, "must write")
+		// fill up the first file
+		_, err = w.Write([]byte(strings.Repeat("a", max)))
+		require.NoError(t, err, "must write")
 
-	// 	// write more, should create a new file
-	// 	_, err = w.Write([]byte("b"))
-	// 	require.NoError(t, err)
+		// write more, should create a new file
+		_, err = w.Write([]byte("b"))
+		require.NoError(t, err)
 
-	// 	require.NoError(t, w.Close())
+		require.NoError(t, w.Close())
 
-	// 	files, err := os.ReadDir(dir)
-	// 	require.NoError(t, err)
+		files, err := os.ReadDir(dir)
+		require.NoError(t, err)
 
-	// 	require.Len(t, files, 2, "must produce 2 files")
-	// })
+		require.Len(t, files, 2, "must produce 2 files")
+	})
 
-	// t.Run("rotates on lifetime", func(t *testing.T) {
-	// 	dir, cleanup := setup(t)
-	// 	defer cleanup()
+	t.Run("rotates on lifetime", func(t *testing.T) {
+		dir, cleanup := setup(t)
+		defer cleanup()
 
-	// 	lifetime := time.Second
-	// 	w, err := New(logger, Options{
-	// 		Directory:       dir,
-	// 		MaximumLifetime: lifetime,
-	// 	})
-	// 	require.NoError(t, err)
+		lifetime := time.Second
+		w, err := New(logger, Options{
+			Directory:       dir,
+			MaximumLifetime: lifetime,
+		})
+		require.NoError(t, err)
 
-	// 	// keep writing until lifetime + half of lifetime (middle of ticks) elapses
-	// 	end := time.Now().Add(lifetime + lifetime/2)
-	// 	for time.Now().Before(end) {
-	// 		_, err = w.Write([]byte("message"))
-	// 		require.NoError(t, err)
-	// 	}
+		// keep writing until lifetime + half of lifetime (middle of ticks) elapses
+		end := time.Now().Add(lifetime + lifetime/2)
+		for time.Now().Before(end) {
+			_, err = w.Write([]byte("message"))
+			require.NoError(t, err)
+		}
 
-	// 	require.NoError(t, w.Close())
-	// 	files, err := os.ReadDir(dir)
-	// 	require.NoError(t, err)
-	// 	require.Len(t, files, 2, "should produce 2 files")
-	// })
+		require.NoError(t, w.Close())
+		files, err := os.ReadDir(dir)
+		require.NoError(t, err)
+		require.Len(t, files, 2, "should produce 2 files")
+	})
 
-	// t.Run("concurrent writes", func(t *testing.T) {
-	// 	dir, cleanup := setup(t)
-	// 	defer cleanup()
+	t.Run("concurrent writes", func(t *testing.T) {
+		dir, cleanup := setup(t)
+		defer cleanup()
 
-	// 	w, err := New(logger, Options{
-	// 		Directory: dir,
-	// 	})
-	// 	require.NoError(t, err)
-	// 	rows := 10000
-	// 	writers := 10
-	// 	messageSize := 10
+		w, err := New(logger, Options{
+			Directory: dir,
+		})
+		require.NoError(t, err)
+		rows := 10000
+		writers := 10
+		messageSize := 10
 
-	// 	var wg sync.WaitGroup
-	// 	for i := 0; i < writers; i++ {
-	// 		wg.Add(1)
-	// 		go func(i int) {
-	// 			for j := 0; j < rows; j++ {
-	// 				_, err := w.Write([]byte(strings.Repeat(fmt.Sprintf("%d", i), messageSize)))
-	// 				require.NoError(t, err)
-	// 			}
-	// 			wg.Done()
-	// 		}(i)
-	// 	}
+		var wg sync.WaitGroup
+		for i := 0; i < writers; i++ {
+			wg.Add(1)
+			go func(i int) {
+				for j := 0; j < rows; j++ {
+					_, err := w.Write([]byte(strings.Repeat(fmt.Sprintf("%d", i), messageSize)))
+					require.NoError(t, err)
+				}
+				wg.Done()
+			}(i)
+		}
 
-	// 	wg.Wait()
-	// 	require.NoError(t, w.Close(), "must close")
+		wg.Wait()
+		require.NoError(t, w.Close(), "must close")
 
-	// 	files, err := os.ReadDir(dir)
-	// 	require.NoError(t, err)
-	// 	require.Len(t, files, 1, "must write a single file")
-	// 	fileinfo, err := files[0].Info()
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, int64(rows*writers*messageSize), fileinfo.Size(), "must write all bytes")
-	// })
+		files, err := os.ReadDir(dir)
+		require.NoError(t, err)
+		require.Len(t, files, 1, "must write a single file")
+		fileinfo, err := files[0].Info()
+		require.NoError(t, err)
+		require.Equal(t, int64(rows*writers*messageSize), fileinfo.Size(), "must write all bytes")
+	})
 
 	t.Run("flush after every write", func(t *testing.T) {
 		dir, cleanup := setup(t)
